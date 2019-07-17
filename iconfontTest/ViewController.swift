2d93f1210e574fc5384686bd56187dddd90744f4
Seven Wang <seven.wang@supernovachina.com>
2019-07-17 13:42:42 +0800

    Initial Commit

---
+++ iconfontTest/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  iconfontTest
+//
+//  Created by Supernova SanDick SSD on 2019/7/17.
+//  Copyright © 2019 Seven. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
